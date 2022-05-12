23d7ad0238955ac03e6c216ae30d9cec773d852c
mewbird <stalol.steam.77@gmail.com>
2022-05-12 09:47:51 +0200

    Fixed RDS and added ANPEQ15 + railed torch & laser

---
+++ lua/arc9/common/attachments/saa_anpeq15_rail.lua
@@ -0,0 +1,79 @@
+ATT.PrintName = "AN-PEQ 15 Laser-Sight System"
+ATT.Description = [[
+Tactical Rail Device composed of both a laser-sight and a flashlight. 
+]]
+ATT.SortOrder = 0
+ATT.Category = {"saa_rail_tactical"}
+ATT.Model = "models/saa/upgrades/arc9/a_anpeq15_rail.mdl"
+ATT.Folder = "LIGHT"
+
+ATT.AimDownSightsTimeMult = 1.03
+ATT.SprintToFireTimeMult = 1.03
+
+ATT.ToggleOnF = true -- This attachment is toggleable with the flashlight key.
+ATT.ToggleStats = {
+    {
+        PrintName = "Both",
+
+        Laser = true,
+        LaserStrength = 1.3,
+        LaserColor = Color(255, 30, 0),
+        LaserAttachment = 1,
+        SwayMult = 1.02,
+        FreeAimRadiusMultHipFire = 0.75,
+
+        Flashlight = true,
+        FlashlightColor = Color(105, 100, 90),
+        FlashlightMaterial = "effects/flashlight001",
+        FlashlightDistance = 750,
+        FlashlightFOV = 70,
+        FlashlightAttachment = 1,
+
+        Flare = true,
+        FlareColor = Color(55, 0, 0),
+        FlareSize = 50,
+        FlareAttachment = 1,
+        FlareFocus = true
+    },
+    {
+        PrintName = "Laser",
+
+        Laser = true,
+        LaserStrength = 1.3,
+        LaserColor = Color(255, 30, 0),
+        LaserAttachment = 1,
+        SwayMult = 1.02,
+        FreeAimRadiusMultHipFire = 0.75,
+
+        Flare = true,
+        FlareColor = Color(55, 0, 0),
+        FlareSize = 50,
+        FlareAttachment = 1,
+        FlareFocus = true
+    },
+    {
+        PrintName = "Flashlight",
+
+        Flashlight = true,
+        FlashlightColor = Color(100, 100, 90),
+        FlashlightMaterial = "effects/flashlight001",
+        FlashlightDistance = 750,
+        FlashlightFOV = 70,
+        FlashlightAttachment = 1,
+        
+        Flare = true,
+        FlareColor = Color(55, 0, 0),
+        FlareSize = 50,
+        FlareAttachment = 1,
+        FlareFocus = true
+    },
+    {
+        PrintName = "None",
+    }
+}
+
+
+ATT.Scale = 1
+
+ATT.ModelOffset = Vector(-0.15, 0, 1.5)
+ATT.ModelAngleOffset = Angle(0, 0, 180)
--- lua/arc9/common/attachments/saa_barska.lua
+++ lua/arc9/common/attachments/saa_barska.lua
@@ -1,4 +1,4 @@
-ATT.PrintName = "AP Comp-M4 Red Dot"
+ATT.PrintName = "Barska Red Dot Sight"
 ATT.Description = [[
 Holographic Sight with no Magnification with a red-dot reticule.
 
@@ -13,12 +13,12 @@ ATT.Folder = "REFLEX"
 ATT.Category = {"SAA_SCOPE_SMALL", "optic_picatinny"}
 
 ATT.ModelAngleOffset = Angle(0, 0, 0)
-ATT.Model = "models/saa/upgrades/a_optic_apcompm4.mdl"
-ATT.ModelOffset = Vector(0.1, 0.15, -0.2)
+ATT.Model = "models/saa/upgrades/a_optic_barska.mdl"
+ATT.ModelOffset = Vector(0.1, 0.15, -0.25)
 ATT.Scale = 1.2
 ATT.Sights = {
     {
-        Pos = Vector(0, 6, -1.2),
+        Pos = Vector(0, 6, -1.4),
         Ang = Angle(0, 0, 0),
         Magnification = 1,
         IgnoreExtra = true
