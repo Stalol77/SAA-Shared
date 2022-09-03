77e4dc51021fbb8565bad58b7a7e963dab847ea3
mewbird <stalol.steam.77@gmail.com>
2022-09-03 15:41:28 +0200

    added xleris scope

---
+++ lua/arc9/common/attachments/saa_xleris.lua
@@ -0,0 +1,67 @@
+ATT.PrintName = "Xleris XM-25"
+ATT.Description = [[
+Battle scope manufactured for multiple distances. Triple-stage Variable x6-x2 Magnification with base thermal system.
+]]
+
+ATT.SortOrder = 4
+ATT.Free = false
+ATT.Folder = "SCOPE"
+
+ATT.Category = {"SAA_SCOPE_MEDIUM", "optic_picatinny"}
+
+ATT.ModelAngleOffset = Angle(0, 0, 0)
+ATT.Model = "models/saa/upgrades/arc9/a_optic_xleris.mdl"
+ATT.ModelOffset = Vector(0.1, 0.15, -0.2)
+ATT.Scale = 1.2
+ATT.Sights = {
+    {
+        Pos = Vector(0, 6, -1.67),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1,
+        IgnoreExtra = true,
+    },
+}
+
+ATT.RTScope = true
+ATT.RTScopeSubmatIndex = 2
+ATT.RTScopeFOV = 22/6
+ATT.RTScopeFOVMax = 22/6
+ATT.RTScopeFOVMin = 22/2
+ATT.RTScopeAdjustmentLevels = 3
+ATT.RTScopeAdjustable = true
+
+ATT.RTScopeFLIR = true
+ATT.RTScopeFLIRSolid = false -- Solid color FLIR instead of like a shaded look
+ATT.RTScopeFLIRRange = 30000
+ATT.RTScopeFLIRMonochrome = true
+ATT.RTScopeFLIRBlend = 0.1
+ATT.RTScopeFLIRCCHot = { -- Color correction drawn only on FLIR targets
+    ["$pp_colour_addr"] = 0,
+    ["$pp_colour_addg"] = 0,
+    ["$pp_colour_addb"] = 0,
+    ["$pp_colour_brightness"] = 0.35,
+    ["$pp_colour_contrast"] = 1,
+    ["$pp_colour_colour"] = 1,
+    ["$pp_colour_mulr"] = 0,
+    ["$pp_colour_mulg"] = 0,
+    ["$pp_colour_mulb"] = 1
+}
+ATT.RTScopeFLIRCCCold = {
+    ["$pp_colour_addr"] = 0,
+    ["$pp_colour_addg"] = 0,
+    ["$pp_colour_addb"] = 0,
+    ["$pp_colour_brightness"] = 0,
+    ["$pp_colour_contrast"] = 0.35,
+    ["$pp_colour_colour"] = 1,
+    ["$pp_colour_mulr"] = 0,
+    ["$pp_colour_mulg"] = 0,
+    ["$pp_colour_mulb"] = 0
+}
+
+
+ATT.RTScopeRes = 512
+ATT.RTScopeSurface = Material("models/weapons/arc9/bo2/bo2_optics/hamr_lens")
+ATT.RTScopeReticle = Material("hud/arc9_saa/reticles/xm25.png", "mips smooth")
+ATT.RTScopeReticleScale = 1.3
+ATT.RTScopeShadowIntensity = 10
+ATT.RTScopeNoPP = false
