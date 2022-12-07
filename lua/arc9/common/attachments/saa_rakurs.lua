4bf1dd8a20922440fe531aecb7e33e10f2c71472
OpticaJr <rcharge617@gmail.com>
2022-12-08 02:23:17 +0300

    rakurs

---
+++ lua/arc9/common/attachments/saa_rakurs.lua
@@ -0,0 +1,30 @@
+ATT.PrintName = "Rakurs-PM1"
+ATT.Description = [[Modern russian sight.]]
+ATT.Pros = {
+    "Clearer Sight Picture"
+}
+ATT.SortOrder = 4
+
+ATT.Model = "models/saa/upgrades/arc9/scope_rakurs.mdl"
+ATT.Folder = "REFLEX"
+
+ATT.Category = {"SAA_SCOPE_SMALL", "optic_picatinny"}
+
+// Allows a custom sight position to be defined
+ATT.Sights = {
+    {
+        Pos = Vector(0, 7.5, -1.8),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.25
+    }
+}
+
+ATT.HoloSight = true
+ATT.HoloSightReticle = Material("models/weapons/optics/rakurs_reticle.png", "mips smooth")
+ATT.HoloSightReticle:SetInt("$additive", 1)
+ATT.HoloSightSize = 1024
+ATT.HoloSightColorable = false
+
+ATT.ModelOffset = Vector(-1, 0.1, -1)
+
+ATT.Scale = Vector(1.1, 1.1, 1.1)
